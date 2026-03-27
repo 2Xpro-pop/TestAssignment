c55ce0dff2b85da21b5eee68828d18be21e2aa43
Asanov <Asanov@DESKTOP-90N0PL6>
2026-03-27 07:09:41 +0600

    feat(apphost): add YARP reverse proxy and route configuration

--- src/TestAssignment.AppHost/AppHost.cs
+++ src/TestAssignment.AppHost/AppHost.cs
@@ -1,4 +1,5 @@
 using Microsoft.Extensions.Hosting;
+using TestAssignment.AppHost;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
@@ -9,22 +10,28 @@ if (builder.Environment.IsProduction())
     postgres.WithPassword(builder.AddParameter("postgres-password", secret: true));
 }
 
+postgres.WithPgAdmin();
+
+var yarp = builder.AddYarp("yarp");
+
 var identityDb = postgres.AddDatabase("identitydb");
 var paymentDb = postgres.AddDatabase("paymentdb");
 
-builder.AddProject<Projects.TestAssignment_Web>("webfrontend")
-    .WithExternalHttpEndpoints()
+var webfrontend = builder.AddProject<Projects.TestAssignment_Web>("webfrontend")
     .WithHttpHealthCheck("/health");
 
 var identityApi = builder.AddProject<Projects.TestAssignment_IdentityApi>("testassignment-identityapi")
     .WithReference(identityDb).WaitFor(identityDb)
     .WithHttpHealthCheck("/health");
 
-builder.AddProject<Projects.TestAssignment_PaymentApi>("testassignment-paymentapi")
+var paymentApi = builder.AddProject<Projects.TestAssignment_PaymentApi>("testassignment-paymentapi")
     .WithReference(paymentDb)
     .WaitFor(paymentDb)
     .WithReference(identityApi)
     .WaitFor(identityApi)
     .WithHttpHealthCheck("/health");
+
+yarp.ConfigureTestAssignmentRoutes(identityApi, paymentApi, webfrontend)
+    .WithExternalHttpEndpoints();
 
 builder.Build().Run();
---
+++ src/TestAssignment.AppHost/YarpExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Aspire.Hosting.Yarp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestAssignment.AppHost;
+
+public static class YarpExtensions
+{
+    public static IResourceBuilder<YarpResource> ConfigureTestAssignmentRoutes(
+        this IResourceBuilder<YarpResource> builder,
+        IResourceBuilder<ProjectResource> identityApi,
+        IResourceBuilder<ProjectResource> paymentApi,
+        IResourceBuilder<ProjectResource> webFrontend)
+    {
+        return builder.WithConfiguration(yarp =>
+        {
+            var identityCluster = yarp.AddCluster(identityApi);
+            var paymentCluster = yarp.AddCluster(paymentApi);
+            var webFrontendCluster = yarp.AddCluster(webFrontend);
+
+            yarp.AddRoute("/api/identity/login", identityCluster)
+                .WithMatchMethods("POST");
+
+            yarp.AddRoute("/api/identity/logout", identityCluster)
+                .WithMatchMethods("POST");
+
+            yarp.AddRoute("/api/payment", paymentCluster)
+                .WithMatchMethods("POST");
+
+            yarp.AddRoute("/{**catch-all}", webFrontendCluster);
+        });
+    }
+}
